d4ec79bdf293afb48164f11fb6cc0a5a627c044a
ImrulEmon <emimrulemon@gmail.com>
2022-12-13 13:40:59 +0600

    Home and start details screen

---
+++ lib/widgets/product_item_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+
+class ProductItemWidget extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItemWidget({Key key, this.id, this.title, this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black38,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
